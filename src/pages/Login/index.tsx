a5db9a48305b98b690f7e95e539f4ff09bc7d56c
Lucas Martins <lm.m4rtins@gmail.com>
2020-08-10 11:55:51 -0300

    commit test

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import { Container } from './styles';
 
 const Login: React.FC = () => {
-  return <div>Login</div>;
+  return <div>Login Page</div>;
 }
 
 export default Login;
