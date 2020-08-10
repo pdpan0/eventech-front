683b9ba8095842eee793366e23382b40b06027e4
Lucas Martins <lm.m4rtins@gmail.com>
2020-08-10 11:53:30 -0300

    Struturing and modeling routes with typescript.

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import { Container } from './styles';
 
 const Login: React.FC = () => {
-  return <div>Login Page</div>;
+  return <div>Login</div>;
 }
 
 export default Login;
